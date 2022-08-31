893f9c93af6426a36ccd1896930ad0f3d1d3dc17
victd <55512729+victd@users.noreply.github.com>
2022-08-31 09:12:30 -0400

    Update break.py

--- break.py
+++ break.py
@@ -14,7 +14,7 @@ doc = docx.Document()
 
 # CHANGE THIS TEXT FILE FOR THE DIVISION ----------------------------
 # are you able to change the format with python and save as pdf as well (using docx2pdf)
-# export directly to write to a PDF file with page breaks, bypass the word doc
+# export directly to write to a PDF file with page breaks (import FPDF), bypass the word doc
 # its an IT initiative, although Skillen only asks for it quarterly
 
 f = open("Barrie employee abstracts from MTO - Jan 2020.txt", "r")
